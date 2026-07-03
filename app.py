9f281545150aba51e2fd3cfd4bc67b422cd0f28a
markusmelt <84225004+markusmelt@users.noreply.github.com>
2026-07-03 15:40:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ with tab1:
                 x=0.02,
                 y=0.98,
                 xanchor="left",
-                yanchor="bottom",
+                yanchor="top",
                 bgcolor="rgba(0, 0, 0, 0)", # Completamente trasparente
                 font=dict(color="blue")     
             )
